4f474be3dc503e156097fed9a0b16c8a9aba3479
Andrei Stepanenko <ftvkyo@gmail.com>
2025-02-18 12:10:56 +0000

    make rendered circles have variable brightness

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -7,8 +7,11 @@ in vec2 texcoord_frag;
 out vec4 color;
 
 void main() {
-    if (sqrt(texcoord_frag.x * texcoord_frag.x + texcoord_frag.y * texcoord_frag.y) < 1.0) {
-        color = vec4(1.0);
+    float d = sqrt(texcoord_frag.x * texcoord_frag.x + texcoord_frag.y * texcoord_frag.y);
+
+    if (d < 1.0) {
+        float brightness = cos(d * 3.1415 / 2);
+        color = vec4(vec3(brightness), 1.0);
     } else {
         color = vec4(0.0);
     }
