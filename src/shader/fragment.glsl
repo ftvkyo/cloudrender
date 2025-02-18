63f42ba03278b3084ba9ceecff9f7a158c544687
Andrei Stepanenko <ftvkyo@gmail.com>
2025-02-18 23:51:27 +0000

    use length() function in the fragment shader

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -7,7 +7,7 @@ in vec2 texcoord_frag;
 out vec4 color;
 
 void main() {
-    float d = sqrt(texcoord_frag.x * texcoord_frag.x + texcoord_frag.y * texcoord_frag.y);
+    float d = length(texcoord_frag);
 
     if (d < 1.0) {
         float brightness = cos(d * 3.1415 / 2);
